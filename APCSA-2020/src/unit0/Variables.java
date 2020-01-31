1cca1a0adf1b8994c063cc38702f17df99cfa6f6
songr7067 <songr7067@CA-SU-F106-09.sduhsd.lan>
2020-01-31 10:39:58 -0800

    1/31/2020 school update

--- APCSA-2020/src/unit0/Variables.java
+++ APCSA-2020/src/unit0/Variables.java
@@ -11,6 +11,7 @@ public class Variables
 {
 	public static void main ( String[] args )
 	{
+		System.out.println("Raymond\t 01/31/2020");
 		//define 1 variable of each of the
 		//following data types
 		//byte		short		int 		long
@@ -21,15 +22,17 @@ public class Variables
 		byte byteOne = 127;
 		short shortOne = Short.MAX_VALUE;
 		int intOne = Integer.MAX_VALUE;
+		long longOne = Long.MAX_VALUE;
 		
 		//decimal variables
-
-
-		//other integer types
-
+		float floatOne = Float.MAX_VALUE;
+		double doubleOne = Double.MAX_VALUE;
+		
 
 		//other types
-
+		char charOne = 'A';
+		boolean boolOne = true;
+		String stringOne =  "My mother is a fish";
 
 		//output your information here
 		System.out.println("/////////////////////////////////");
@@ -38,17 +41,21 @@ public class Variables
 		System.out.println("*                               *");
 		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
 		System.out.println("*16 bit - shortOne = " + shortOne);
-		System.out.println("*16 bit - shortOne = " + shortOne);
-
-
-
-
-
-
-
-
-
-
+		System.out.println("*32 bit - intOne = " + intOne);
+		System.out.println("*64 bit - longOne = " + longOne);
+		System.out.println("/////////////////////////////////");
+		System.out.println("*                               *");
+		System.out.println("*        decimal types          *");
+		System.out.println("*                               *");
+		System.out.println("Float :) = " + floatOne);
+		System.out.println("Double :) = " + doubleOne);
+		System.out.println("/////////////////////////////////");
+		System.out.println("*                               *");
+		System.out.println("*        other types          *");
+		System.out.println("*                               *");
+		System.out.println("Character = " + charOne);
+		System.out.println("Boolean = " + boolOne);
+		System.out.println("String = " + stringOne);
 
 
 	}
