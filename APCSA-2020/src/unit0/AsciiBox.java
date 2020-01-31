8daadbac0e4a1d8a152e7ac0977d81054de70601
songr7067 <songr7067@CA-SU-F106-09.sduhsd.lan>
2020-01-31 10:26:06 -0800

    upload unit zero from school

---
+++ APCSA-2020/src/unit0/AsciiBox.java
@@ -0,0 +1,22 @@
+package unit0;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Raymond Song \t  1/30/2020 \n\n" );
+		System.out.println();
+		for (int i = 0; i < 10; i++) {
+			System.out.println("+++++++++++++++++++++++++ " );
+			System.out.println("---------------------------- ");
+		}
+
+	}
+}
---
+++ APCSA-2020/src/unit0/Variables.java
@@ -0,0 +1,55 @@
+package unit0;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Variables
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteOne = 127;
+		short shortOne = Short.MAX_VALUE;
+		int intOne = Integer.MAX_VALUE;
+		
+		//decimal variables
+
+
+		//other integer types
+
+
+		//other types
+
+
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*16 bit - shortOne = " + shortOne);
+		System.out.println("*16 bit - shortOne = " + shortOne);
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
