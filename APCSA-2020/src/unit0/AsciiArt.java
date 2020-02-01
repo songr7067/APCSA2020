8d41a44b54bf245049588a2db91e21bb9b3e8c21
battl <battl@100.64.94.1>
2020-02-01 09:49:24 -0800

    ACSIIART and Input updates

--- APCSA-2020/src/unit0/AsciiArt.java
+++ APCSA-2020/src/unit0/AsciiArt.java
@@ -1,24 +1,24 @@
 package unit0;
-//(c) A+ Computer Science
-//www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  -
 
 public class AsciiArt
 {
 	public static void main ( String[] args )
 	{
-		System.out.println("Your Name \n\n" );
+		System.out.println("Raymond Song \n\n" );
 		System.out.println("What type of ANIMAL YOU WILL DRAW" );
 		System.out.println("\n\n\n\n" );
 
-		System.out.println("                /\\                " );
-		System.out.println("               /  \\               " );
-		System.out.println("              /    \\              " );
-		System.out.println("             [------]             " );
+		System.out.println("            ||||        ||||         " );
+		System.out.println("            ||||        ||||         " );
+		System.out.println("            ||||        ||||         " );
+		System.out.print("\n\n\n\n\n\n" );
+		System.out.println("       \\                       /" );
+		System.out.println("         \\                   /" );
+		System.out.println("           \\               /" );
+		System.out.println("             \\           /" );
+		System.out.println("               \\       /" );
+		System.out.println("                 -------             " );
 		//add other output
 
 		System.out.println(" \n\n\n\nHelpFul Hints" );
--- APCSA-2020/src/unit0/Input.java
+++ APCSA-2020/src/unit0/Input.java
@@ -25,19 +25,38 @@ public class Input
 		System.out.print("Enter an integer :: ");
 		intOne = keyboard.nextInt();
 
-
 		System.out.print("Enter an integer :: ");
 		intTwo = keyboard.nextInt();
 
+		System.out.println("Enter a double :: ");
+		doubleOne = keyboard.nextDouble();
+		
+		System.out.println("Enter a double :: ");
+		doubleTwo = keyboard.nextDouble();
 
+		System.out.println("Enter a float :: ");
+		floatOne = keyboard.nextFloat();
+		
+		System.out.println("Enter a float :: ");
+		floatTwo = keyboard.nextFloat();
+		
+		System.out.println("Enter a short :: ");
+		shortOne = keyboard.nextShort();
+		
+		System.out.println("Enter a short :: ");
+		shortTwo = keyboard.nextShort();
 		//add in input for all variables
 
 
 		System.out.println();
 		System.out.println("integer one = " + intOne );
 		System.out.println("integer two = " + intTwo );
-
-		//add in output for all variables
+		System.out.println("double one = " + doubleOne);
+		System.out.println("double two = " + doubleTwo);
+		System.out.println("float one = " + floatOne);
+		System.out.println("float two = " + floatTwo);
+		System.out.println("short one = " + shortOne);
+		System.out.println("short two = " + shortTwo);
 
 
 	}
