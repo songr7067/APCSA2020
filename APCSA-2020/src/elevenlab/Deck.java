016935788fe472a94639f8c59f8d2a1b228f20da
songr7067 <songr7067@CA-SU-F106-09.sduhsd.lan>
2020-02-26 11:51:38 -0800

    Deck fix :((((()))))

--- APCSA-2020/src/elevenlab/Deck.java
+++ APCSA-2020/src/elevenlab/Deck.java
@@ -3,6 +3,7 @@ package elevenlab;
 //www.apluscompsci.com
 //Name -
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -19,7 +20,7 @@ public class Deck
    //make a Deck constructor
 	public Deck() {
 		top = 51;
-	
+	cards = new ArrayList<Card>();
    	//refer cards to new ArrayList
    	//set top to the top of the deck 51
    	
@@ -28,7 +29,7 @@ public class Deck
    			//add a new TwentyOneCard to the deck
 		for (int i =0; i<4;i++) {
 			for (int k =1;k<=13;k++){
-				cards.add(new Card(SUITS[i],k));
+				cards.add(0,new Card(SUITS[i],k));
 			}
 		}
 	}
