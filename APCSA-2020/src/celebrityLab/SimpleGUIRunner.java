76393cc801bf637998bce22a3f4b54ce681b13b9
songr7067 <songr7067@CA-SU-F106-09.sduhsd.lan>
2020-03-09 10:56:54 -0700

    Uploaded Celebrity Lab

---
+++ APCSA-2020/src/celebrityLab/SimpleGUIRunner.java
@@ -0,0 +1,12 @@
+package celebrityLab;
+import javax.swing.JFrame;
+
+public class SimpleGUIRunner
+{
+	public static void main(String[] args)
+	{
+		JFrame sample = new JFrame();
+		sample.setVisible(true);
+	}
+
+}
